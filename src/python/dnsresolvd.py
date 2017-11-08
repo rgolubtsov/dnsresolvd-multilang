aad111878e53b4963767f9eb9bbb8f6acd6c9165
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2017-11-08 18:53:00 +1100

    twisted: Enrich code commentary, call the method to write headers to the response, encode the response buffer returned.

--- src/python/dnsresolvd.py
+++ src/python/dnsresolvd.py
@@ -17,16 +17,42 @@ from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet           import reactor
 from twisted.web.server         import Site
 
+from dns_resolv.controller_helper     import ControllerHelper
 from dns_resolv.dns_lookup_controller import DnsLookupController
 
 ## The main class of the daemon.
 class DnsResolvd:
-    ## Starts up the daemon.
+    ##
+    # Starts up the daemon.
+    #
+    # @param port_number The server port number to listen on.
+    # @param daemon_name The daemon name (executable/script name).
+    #
+    # @return The server exit code when interrupted.
+    #
     def startup(self, port_number, daemon_name):
+        # Instantiating the controller helper class.
+        aux = ControllerHelper()
+
+        ret = aux._EXIT_SUCCESS
+
+        ##
+        # The server class of the daemon.
+        # Used by Twisted engine to run an event loop.
+        #
+        # @extends Resource The Twisted web-accessible resource class.
+        #
         class Daemon(Resource):
             isLeaf = True
 
-            def render_GET(self, request):
+            ##
+            # Renders the HTTP response based on the incoming HTTP request.
+            #
+            # @param req The incoming HTTP request object.
+            #
+            # @return The HTTP response has to be rendered.
+            #
+            def render_GET(self, req):
                 # Instantiating the controller class.
                 ctrl = DnsLookupController()
 
@@ -39,10 +65,13 @@ class DnsResolvd:
                 # -------------------------------------------------------------
                 resp_buffer = ("<!DOCTYPE html><html><head><title>" + dbg
                             +  "</title></head><body><p>"           + dbg
-                            +  "</p></body></html>")
+                            +  "</p></body></html>").encode()
                 # -------------------------------------------------------------
                 # --- Debug output - End --------------------------------------
                 # -------------------------------------------------------------
+
+                # Adding headers to the response.
+                aux.add_response_headers(req)
 
                 return resp_buffer
 
@@ -52,8 +81,13 @@ class DnsResolvd:
 
                 return None
 
+        # Setting up the TCP IPv4-configured server.
         TCP4ServerEndpoint(reactor, port_number).listen(Site(Daemon()))
-        reactor.run()
+
+        # Running the event loop.
+        ret = reactor.run()
+
+        return ret
 
     ## Default constructor.
     def __init__(self):
