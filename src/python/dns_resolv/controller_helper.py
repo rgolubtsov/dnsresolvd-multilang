174acdb7ec2240d72a22e984002f0c9c684c8843
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2018-03-21 13:50:00 +1100

    twisted: Implement JSON output (default).

--- src/python/dns_resolv/controller_helper.py
+++ src/python/dns_resolv/controller_helper.py
@@ -60,15 +60,26 @@ class ControllerHelper:
     _MSG_SERVER_STARTED_1 = "Server started on port "
     _MSG_SERVER_STARTED_2 = "=== Hit Ctrl+C to terminate it."
 
+    # HTTP request params.
+    _PRM_FMT_HTML = "html"
+    _PRM_FMT_JSON = "json"
+
     # HTTP response headers.
-    _HDR_CONTENT_TYPE_N  = "Content-Type"
-    _HDR_CONTENT_TYPE_V  = "text/html; charset=UTF-8"
-    _HDR_CACHE_CONTROL_N = "Cache-Control"
-    _HDR_CACHE_CONTROL_V = "no-cache, no-store, must-revalidate"
-    _HDR_EXPIRES_N       = "Expires"
-    _HDR_EXPIRES_V       = "Thu, 01 Dec 1994 16:00:00 GMT"
-    _HDR_PRAGMA_N        = "Pragma"
-    _HDR_PRAGMA_V        = "no-cache"
+    _HDR_CONTENT_TYPE_N      = "Content-Type"
+    _HDR_CONTENT_TYPE_V_HTML = "text/html; charset=UTF-8"
+    _HDR_CONTENT_TYPE_V_JSON = "application/json"
+    _HDR_CACHE_CONTROL_N     = "Cache-Control"
+    _HDR_CACHE_CONTROL_V     = "no-cache, no-store, must-revalidate"
+    _HDR_EXPIRES_N           = "Expires"
+    _HDR_EXPIRES_V           = "Thu, 01 Dec 1994 16:00:00 GMT"
+    _HDR_PRAGMA_N            = "Pragma"
+    _HDR_PRAGMA_V            = "no-cache"
+
+    # Response data names.
+    _DAT_HOSTNAME_N = "hostname"
+    _DAT_ADDRESS_N  = "address"
+    _DAT_VERSION_N  = "version"
+    _DAT_VERSION_V  = "IPv"
 
     # Daemon name, version, and copyright banners.
     _DMN_NAME        =  "DNS Resolver Daemon (dnsresolvd)"
@@ -82,14 +93,20 @@ class ControllerHelper:
     ## Constant: The default hostname to look up for.
     _DEF_HOSTNAME = "openbsd.org"
 
-    def add_response_headers(self, req):
+    def add_response_headers(self, req, fmt):
         """Adds headers to the response.
 
         Args:
             req: The incoming HTTP request object.
+            fmt: The response format selector.
         """
 
-        req.setHeader(self._HDR_CONTENT_TYPE_N,  self._HDR_CONTENT_TYPE_V )
+        if   (fmt == self._PRM_FMT_HTML):
+            HDR_CONTENT_TYPE_V = self._HDR_CONTENT_TYPE_V_HTML
+        elif (fmt == self._PRM_FMT_JSON):
+            HDR_CONTENT_TYPE_V = self._HDR_CONTENT_TYPE_V_JSON
+
+        req.setHeader(self._HDR_CONTENT_TYPE_N,        HDR_CONTENT_TYPE_V )
         req.setHeader(self._HDR_CACHE_CONTROL_N, self._HDR_CACHE_CONTROL_V)
         req.setHeader(self._HDR_EXPIRES_N,       self._HDR_EXPIRES_V      )
         req.setHeader(self._HDR_PRAGMA_N,        self._HDR_PRAGMA_V       )
