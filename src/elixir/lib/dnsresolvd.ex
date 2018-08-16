43f2abcc6c16538e3e3ceed046f93057832d9d9f
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2018-08-16 15:40:30 +1000

    cowboy/elixir: Reformat, reshape, populate... a little.))

--- src/elixir/lib/dnsresolvd.ex
+++ src/elixir/lib/dnsresolvd.ex
@@ -1,18 +1,40 @@
+#
+# src/elixir/lib/dnsresolvd.ex
+# =============================================================================
+# DNS Resolver Daemon (dnsresolvd). Version 0.1
+# =============================================================================
+# A daemon that performs DNS lookups for the given hostname
+# passed in an HTTP request, with the focus on its implementation
+# using various programming languages. (Cowboy-boosted impl.)
+# =============================================================================
+# Copyright (C) 2017-2018 Radislav (Radicchio) Golubtsov
+#
+# (See the LICENSE file at the top of the source tree.)
+#
+
 defmodule Dnsresolvd do
-  @moduledoc """
-  Documentation for Dnsresolvd.
-  """
+    @moduledoc """
+    The main module of the daemon.
+    """
 
-  @doc """
-  Hello world.
+    def _DEF_HOSTNAME, do: "openbsd.org"
 
-  ## Examples
+    @doc """
+    Performs DNS lookup action for the given hostname,
+    i.e. (in this case) IP address retrieval by hostname.
 
-      iex> Dnsresolvd.hello
-      :world
+    **Args:**<br />
+    `hostname`: The effective hostname to look up for.
 
-  """
-  def hello do
-    :world
-  end
+    **Returns:** The array containing IP address of the analyzing host/service
+                 and corresponding IP version (family) used to look up in DNS:
+                 `4` for IPv4-only hosts, `6` for IPv6-capable hosts.
+    """
+    def dns_lookup(hostname) do
+        IO.puts("=== " <> hostname)
+    end
 end
+
+Dnsresolvd.dns_lookup(Dnsresolvd._DEF_HOSTNAME)
+
+# vim:set nu ts=4 sw=4:
