fcffc1211c58a478cbf7621eb2cfbc3294631225
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2018-08-21 18:50:00 +1000

    cowboy/elixir: Check against cmd-line args presence and port correctness.

--- src/elixir/lib/dnsresolvh.ex
+++ src/elixir/lib/dnsresolvh.ex
@@ -22,10 +22,30 @@ defmodule AUX do
     def _COLON_SPACE_SEP , do: ": "
     def _COMMA_SPACE_SEP , do: ", "
     def _NEW_LINE        , do: "\n"
-    def _C_FMT           , do: "%c"
+    def _SLASH           , do:  "/"
     def _AMPER           , do:  "&"
     def _ONE_SPACE_STRING, do:  " "
     def _PRINT_BANNER_OPT, do: "-V"
+
+    # Common error messages.
+    def _ERR_PORT_MUST_BE_POSITIVE_INT, do: ": <port_number> must be "
+                                        <>  "a positive integer value, "
+                                        <>  "in the range 1024-49151."
+
+    # Print this error message when there are no any args passed.
+    def _ERR_MUST_BE_ONE_TWO_ARGS_1, do: ": There must be one or two args "
+                                     <>  "passed: "
+    def _ERR_MUST_BE_ONE_TWO_ARGS_2, do: " args found"
+
+    # Print this usage info just after any inappropriate input.
+    def _MSG_USAGE_TEMPLATE_1, do: "Usage: "
+    def _MSG_USAGE_TEMPLATE_2, do: " <port_number> [-V]"
+
+    # Constant: The minimum port number allowed.
+    def _MIN_PORT, do: 1024
+
+    # Constant: The maximum port number allowed.
+    def _MAX_PORT, do: 49151
 
     # Daemon name, version, and copyright banners.
     def _DMN_NAME       , do: "DNS Resolver Daemon (dnsresolvd)"
@@ -38,6 +58,12 @@ defmodule AUX do
 
     # Constant: The default hostname to look up for.
     def _DEF_HOSTNAME, do: "openbsd.org"
+
+    # Helper function. Makes final buffer cleanups, closes streams, etc.
+    def _cleanups_fixate() do
+        # Closing the system logger.
+        # Posix.closelog()
+    end
 
     # Helper function. Draws a horizontal separator banner.
     def _separator_draw(banner_text) do
