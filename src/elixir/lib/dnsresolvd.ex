3bc68a461e30cbb5fe349cea25a970264332590f
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2018-09-05 19:50:00 +1000

    cowboy/elixir: ===> (1) Replace Process.sleep() call with the inspecting process message queue construct. (2) Mark dns_lookup() function as private. (3) Actualize ExDoc commentary blocks and simply add them where absent.

--- src/elixir/lib/dnsresolvd.ex
+++ src/elixir/lib/dnsresolvd.ex
@@ -18,15 +18,19 @@ defmodule DnsResolvd do
     use Application
 
     @doc """
-    Starts up the daemon.
+    Starts up the daemon.<br />
+    It has to be the application module callback, but used directly
+    from the startup script of the daemon.
 
     **Args:**<br />
         `args`: A list containing the server port number to listen on
                 as the first element.
 
     **Returns:**<br />
-        The server exit code when interrupted.
+        The tuple containing the PID of the top supervisor process
+        and the state of the running application (or an empty list).
     """
+    @impl (true)
     def start(_, args) do
         [port_number | _] = args
 
@@ -51,25 +55,40 @@ defmodule DnsResolvd do
         # Starting up the daemon's provided Supervisor (optional).
         DnsResolvs.start_link()
 
+        # Trapping exit signals, i.e. transforming them into {:EXIT} message.
+        Process.flag(:trap_exit, true)
+
+        # Inspecting the daemon's Application process message queue
+        # for the incoming {:EXIT} message, i.e. doing same things
+        # as the Process.sleep(:infinity) inactive call below
+        # until the message received.
+        receive do
+            {:EXIT, from, reason} ->
+                IO.inspect(from)
+                IO.inspect(reason)
+        end
+
+        # --- Use message queue (as above) instead of sleep() call below ------
         # Using this call instead of passing "--no-halt"
         # to the Elixir interpreter. See the file "dnsresolvd.app"
         # for explanation.
-        Process.sleep(:infinity)
+        # Process.sleep(:infinity)
+        # ---------------------------------------------------------------------
     end
 
-    @doc """
-    Performs DNS lookup action for the given hostname,
-    i.e. (in this case) IP address retrieval by hostname.
-
-    **Args:**<br />
-        `hostname`: The effective hostname to look up for.
-
-    **Returns:**<br />
-        The array containing IP address of the analyzing host/service
-        and corresponding IP version (family) used to look up in DNS:
-        `4` for IPv4-only hosts, `6` for IPv6-capable hosts.
-    """
-    def dns_lookup(hostname) do
+    ###
+    # Performs DNS lookup action for the given hostname,
+    # i.e. (in this case) IP address retrieval by hostname.
+    #
+    # Args:
+    #     hostname: The effective hostname to look up for.
+    #
+    # Returns:
+    #     The array containing IP address of the analyzing host/service
+    #     and corresponding IP version (family) used to look up in DNS:
+    #     "4" for IPv4-only hosts, "6" for IPv6-capable hosts.
+    #
+    defp dns_lookup(hostname) do
         # TODO: Implement performing DNS lookup action for the given hostname.
         #       This function currently is a dummy thing and hence
         #       it should be populated with the actual code.
@@ -84,10 +103,26 @@ defmodule DnsResolvs do
 
     use Supervisor
 
+    @doc """
+    Supervisor startup helper.<br />
+    Used to directly start up the `DnsResolvs` supervisor process.
+
+    **Returns:**<br />
+        The PID of the `DnsResolvs` supervisor process.
+    """
     def start_link() do
         Supervisor.start_link(__MODULE__, [])
     end
 
+    @doc """
+    Supervisor init/1 callback.<br />
+    Gets called when the `DnsResolvs` supervisor is about to be started up.
+
+    **Returns:**<br />
+        The tuple containing supervisor flags
+        and child processes' specifications.
+    """
+    @impl (true)
     def init(_) do
         child_procs = [] # <== No child processes do we need.
 
