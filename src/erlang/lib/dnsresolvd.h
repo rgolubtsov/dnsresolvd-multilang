c31a0df23c8d34ec32811b6d16449a858de8ce29
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2018-09-26 14:30:00 +1000

    cowboy/erlang: Bring out constants to a helper header file; exit from startup script explicitly.

---
+++ src/erlang/lib/dnsresolvd.h
@@ -0,0 +1,42 @@
+%
+% src/erlang/lib/dnsresolvd.h
+% =============================================================================
+% DNS Resolver Daemon (dnsresolvd). Version 0.1
+% =============================================================================
+% A daemon that performs DNS lookups for the given hostname
+% passed in an HTTP request, with the focus on its implementation
+% using various programming languages. (Cowboy-boosted impl.)
+% =============================================================================
+% Copyright (C) 2017-2018 Radislav (Radicchio) Golubtsov
+%
+% (See the LICENSE file at the top of the source tree.)
+%
+
+%% @doc The helper header file for the daemon.
+%%      It aimed at accumulating such pieces like constants, helper functions,
+%%      just like it behaves in its C counterpart. Yep, that's a little funny
+%%      to make it named like in C, but definitely feasible nevertheless!))
+%% @end
+
+% Helper constants.
+-define(_EXIT_FAILURE,          1). %    Failing exit status.
+-define(_EXIT_SUCCESS,          0). % Successful exit status.
+-define(_EMPTY_STRING,         "").
+-define(_COMMA_SPACE_SEP,    ", ").
+-define(_NEW_LINE,           "\n").
+-define(_ONE_SPACE_STRING,    " ").
+-define(_PRINT_BANNER_OPT,   "-V").
+-define(_ESCRIPT_NAME,        "_").
+-define(_PORT_NUMBER0,        "0").
+-define(_DBG_PREF,         "==> ").
+
+% Daemon name, version, and copyright banners.
+-define(_DMN_NAME,        "DNS Resolver Daemon (dnsresolvd)").
+-define(_DMN_DESCRIPTION, "Performs DNS lookups for the given "
+                       ++ "hostname passed in an HTTP request").
+-define(_DMN_VERSION_S__, "Version").
+-define(_DMN_VERSION,     "0.1").
+-define(_DMN_COPYRIGHT__, "Copyright (C) 2017-2018").
+-define(_DMN_AUTHOR,      "Radislav Golubtsov <ragolubtsov@my.com>").
+
+% vim:set nu et ts=4 sw=4:
