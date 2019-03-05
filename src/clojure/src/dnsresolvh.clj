99f7e4c35db6c69ef4afac5130b146a90464a9af
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2019-03-05 19:15:01 +1100

    http-kit/clojure: Compile modules using namespace.

---
+++ src/clojure/src/dnsresolvh.clj
@@ -0,0 +1,17 @@
+;
+; src/clojure/src/dnsresolvh.clj
+; =============================================================================
+; DNS Resolver Daemon (dnsresolvd). Version 0.1
+; =============================================================================
+; A daemon that performs DNS lookups for the given hostname
+; passed in an HTTP request, with the focus on its implementation
+; using various programming languages. (HTTP Kit-boosted impl.)
+; =============================================================================
+; Copyright (C) 2017-2019 Radislav (Radicchio) Golubtsov
+;
+; (See the LICENSE file at the top of the source tree.)
+;
+
+(ns dnsresolvd)
+
+; vim:set nu et ts=4 sw=4:
