56e18a33deb374ca57898051b60fd2cc3a56482a
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2019-06-22 22:50:00 +1000

    vertosphere/java: Continue porting from Vala and Perl: (1) Validate port number and discard any rubbish it may contain. (2) Check args presence. (3) Check port correctness. (4) Draw horizontal separator banner.

--- src/java/DnsResolvd.java
+++ src/java/DnsResolvd.java
@@ -12,7 +12,7 @@
  * (See the LICENSE file at the top of the source tree.)
  */
 
-import dns_resolv.ControllerHelper;
+import static dns_resolv.ControllerHelper.*;
 
 /** The startup class of the daemon. */
 public class DnsResolvd {
@@ -22,8 +22,74 @@ public class DnsResolvd {
      * @param argv The array of command-line arguments.
      */
     public static void main(final String[] argv) {
-        int ret = ControllerHelper.EXIT_SUCCESS;
-//      int ret = ControllerHelper.EXIT_FAILURE;
+        int ret = EXIT_SUCCESS;
+
+        int    argc        = argv.length - 1;
+        String daemon_name = argv[0];
+        int    port_number;
+
+        String print_banner_opt = EMPTY_STRING;
+
+        if (argc > 0) {
+            // Validating the port number and discarding any rubbish
+            // it may contain.
+            try {
+                port_number = new Integer(argv[1]).intValue();
+            } catch (NumberFormatException e) {
+                port_number = 0;
+            }
+
+            if (argc > 1) {
+                print_banner_opt = argv[2].toUpperCase();
+            }
+        } else {
+            port_number = 0;
+        }
+
+        if (print_banner_opt.compareTo(PRINT_BANNER_OPT) == 0) {
+            separator_draw(DMN_DESCRIPTION);
+
+            System.out.println(DMN_NAME + COMMA_SPACE_SEP  + DMN_VERSION_S__
+                     + ONE_SPACE_STRING + DMN_VERSION      + NEW_LINE
+                     + DMN_DESCRIPTION                     + NEW_LINE
+                     + DMN_COPYRIGHT__  + ONE_SPACE_STRING + DMN_AUTHOR);
+
+            separator_draw(DMN_DESCRIPTION);
+        }
+
+        // Checking for args presence.
+        if (argc == 0) {
+            ret = EXIT_FAILURE;
+
+            System.err.println(daemon_name + ERR_MUST_BE_ONE_TWO_ARGS_1
+                                    + argc + ERR_MUST_BE_ONE_TWO_ARGS_2
+                                           + NEW_LINE);
+
+            System.err.println(MSG_USAGE_TEMPLATE_1 + daemon_name
+                             + MSG_USAGE_TEMPLATE_2 + NEW_LINE);
+
+            cleanups_fixate();
+
+            System.exit(ret);
+        }
+
+        // Checking for port correctness.
+        if ((port_number < MIN_PORT) || (port_number > MAX_PORT)) {
+            ret = EXIT_FAILURE;
+
+            System.err.println(daemon_name + ERR_PORT_MUST_BE_POSITIVE_INT
+                                           + NEW_LINE);
+
+            System.err.println(MSG_USAGE_TEMPLATE_1 + daemon_name
+                             + MSG_USAGE_TEMPLATE_2 + NEW_LINE);
+
+            cleanups_fixate();
+
+            System.exit(ret);
+        }
+
+        // Making final cleanups.
+        cleanups_fixate();
 
         System.exit(ret);
     }
