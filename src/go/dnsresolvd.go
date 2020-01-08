861768f00c4933d634f3c60e1de6ca6564200994
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2020-01-08 19:35:30 +1100

    net/http/go: Continue porting the daemon from C and Vala to Go.

--- src/go/dnsresolvd.go
+++ src/go/dnsresolvd.go
@@ -14,12 +14,24 @@
 
 package main
 
+import "os"
+import "strconv"
+import "strings"
 import "fmt"
+import "log/syslog"
+import "path/filepath"
 
+const _EXIT_FAILURE     int    =    1 //    Failing exit status.
+const _EXIT_SUCCESS     int    =    0 // Successful exit status.
 const _EMPTY_STRING     string =   ""
 const _COMMA_SPACE_SEP  string = ", "
 const _NEW_LINE         string = "\n"
 const _ONE_SPACE_STRING string =  " "
+const _PRINT_BANNER_OPT string = "-V"
+const _ERR_MUST_BE_ONE_TWO_ARGS_1 string = ": There must be one or two args passed: "
+const _ERR_MUST_BE_ONE_TWO_ARGS_2 string = " args found"
+const _MSG_USAGE_TEMPLATE_1 string = "Usage: "
+const _MSG_USAGE_TEMPLATE_2 string = " <port_number> [-V]"
 const _DMN_NAME         string = "DNS Resolver Daemon (dnsresolvd)"
 const _DMN_DESCRIPTION  string = "Performs DNS lookups for the given " +
                                  "hostname passed in an HTTP request"
@@ -29,34 +41,31 @@ const _DMN_COPYRIGHT__  string = "Copyright (C) 2017-2020"
 const _DMN_AUTHOR       string = "Radislav Golubtsov <ragolubtsov@my.com>"
 
 // The daemon entry point.
-func main() {/*
-    int ret = EXIT_SUCCESS;
+func main() {
+    var ret int = _EXIT_SUCCESS
 
-    char *daemon_name = argv[0];
-    unsigned short port_number;
+    var argc uint = uint(len(os.Args) - 1)
 
-    char *print_banner_opt = _EMPTY_STRING;
+        daemon_name := os.Args[0]
+    var port_number uint
 
-    int argv2_len, i; *//*
-        ^          ^
-        |          |
-        +----------+--- Needs this for toupper'ing argv[2] only. *//*
+    var print_banner_opt string = _EMPTY_STRING
 
-    SoupServer *dmn = NULL;
-    GMainLoop *loop = NULL;
-    GError   *error = NULL;
+    if (argc > 0) {
+        port_number_, e := strconv.Atoi(os.Args[1])
 
-    if (argc > 2) {
-        argv2_len = strlen(argv[2]);
+        if (e == nil) { port_number = uint(port_number_) }
 
-        print_banner_opt = malloc(argv2_len);
-
-        for (i = 0; i <= argv2_len; i++) {
-            print_banner_opt[i] = toupper(argv[2][i]);
+        if (argc > 1) {
+            print_banner_opt = strings.ToUpper(os.Args[2])
         }
-    }*//*
+    } else {
+        port_number = 0
+    }
 
-    if (strcmp(print_banner_opt, _PRINT_BANNER_OPT) == 0) {*/
+    fmt.Println(port_number)
+
+    if (print_banner_opt == _PRINT_BANNER_OPT) {
         _separator_draw(_DMN_DESCRIPTION)
 
         fmt.Printf(_DMN_NAME   + _COMMA_SPACE_SEP                             +
@@ -64,34 +73,35 @@ func main() {/*
               _DMN_DESCRIPTION +                                    _NEW_LINE +
               _DMN_COPYRIGHT__ + _ONE_SPACE_STRING + _DMN_AUTHOR  + _NEW_LINE)
 
-        _separator_draw(_DMN_DESCRIPTION)/*
+        _separator_draw(_DMN_DESCRIPTION)
     }
 
-    if (argc > 2) {
-        free(print_banner_opt);
-    }*/
+    // Opening the system logger.
+    log, e := syslog.Dial(_EMPTY_STRING,   _EMPTY_STRING,
+                          syslog.LOG_ERR | syslog.LOG_DAEMON,
+                          filepath.Base(daemon_name))
 
-    /* Opening the system logger. *//*
-    openlog(NULL, LOG_CONS | LOG_PID, LOG_DAEMON);
+    if (e == nil) {}
 
-    /* Checking for args presence. *//*
-    if (argc == 1) {
-        ret = EXIT_FAILURE;
+    // Checking for args presence.
+    if (argc == 0) {
+        ret = _EXIT_FAILURE
 
-        fprintf(stderr, _ERR_MUST_BE_ONE_TWO_ARGS _NEW_LINE _NEW_LINE,
-                         daemon_name, (argc - 1));
+        fmt.Fprintf(os.Stderr, daemon_name      +
+                    _ERR_MUST_BE_ONE_TWO_ARGS_1 + strconv.Itoa(int(argc)) +
+                    _ERR_MUST_BE_ONE_TWO_ARGS_2 + _NEW_LINE + _NEW_LINE)
 
-        syslog(LOG_ERR, _ERR_MUST_BE_ONE_TWO_ARGS _NEW_LINE _NEW_LINE,
-                         daemon_name, (argc - 1));
+        log.Err(               daemon_name      +
+                    _ERR_MUST_BE_ONE_TWO_ARGS_1 + strconv.Itoa(int(argc)) +
+                    _ERR_MUST_BE_ONE_TWO_ARGS_2 + _NEW_LINE)
 
-        fprintf(stderr, _MSG_USAGE_TEMPLATE _NEW_LINE _NEW_LINE, daemon_name);
+        fmt.Fprintf(os.Stderr, _MSG_USAGE_TEMPLATE_1 + daemon_name +
+                               _MSG_USAGE_TEMPLATE_2 + _NEW_LINE   + _NEW_LINE)
 
-        _cleanups_fixate(NULL);
+        _cleanups_fixate(log)
 
-        return ret;
+        os.Exit(ret)
     }
-
-    port_number = atoi(argv[1]);*/
 
     /* Checking for port correctness. *//*
     if ((port_number < _MIN_PORT) || (port_number > _MAX_PORT)) {
@@ -176,21 +186,16 @@ func main() {/*
         return ret;
     }*/
 
-    /* Making final cleanups. *//*
-    _cleanups_fixate(loop);
+    // Making final cleanups.
+    _cleanups_fixate(log)
 
-    return ret;*/
+    os.Exit(ret)
 }
 
 // Helper function. Makes final buffer cleanups, closes streams, etc.
-func _cleanups_fixate(/*const gpointer loop*/) {
-    // Stopping the daemon.
-    /*if ((loop != NULL) && (g_main_loop_is_running(loop))) {
-        g_main_loop_quit(loop);
-    }*/
-
+func _cleanups_fixate(log *syslog.Writer) {
     // Closing the system logger.
-    //closelog();
+    log.Close()
 }
 
 // Helper function. Draws a horizontal separator banner.
